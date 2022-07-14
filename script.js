402e33d55325af949e7abff69adfb3123babcda0
Rocio Gallardo <hola.rogallardo@gmail.com>
2022-07-14 15:04:22 -0300

    agrego commentarios

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//CLASS PARA LUEGO CREAR LOS OBJETOS
+
 class Pelicula{
     constructor(nombre, año, categoria, puntuacion, duracion){
         this.nombre = nombre
@@ -8,6 +10,7 @@ class Pelicula{
 }
 }
 
+// OBJETOS CON PELÍCULAS, LAS CATEGORÍAS  SON UN ARRAY
 const pelicula1 = new Pelicula("El juicio de los 7 de Chicago", 2020, ["drama"], 89, 128 )
 const pelicula2 = new Pelicula("Claroscuro", 2021, ["drama"], 90, 99)
 const pelicula3 = new Pelicula("La calle del terror : parte 3 - 1666", 2021, ["sobrenatural"], 89, 115)
@@ -36,11 +39,11 @@ const pelicula25 = new Pelicula("Casa ajena", 2020, ["terror, suspenso"], 100, 9
 
 const peliculas = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5, pelicula6, pelicula7, pelicula8, pelicula9, pelicula10, pelicula11, pelicula12, pelicula13, pelicula14, pelicula15, pelicula16, pelicula17, pelicula18, pelicula19, pelicula20, pelicula21, pelicula22, pelicula23, pelicula24, pelicula25]
 
-
+//PRIMER ALERT CON EXPLICACIÓN
 alert("Hola ! tenemos una lista de las 25 películas de Netflix mejor valoradas por la crítica")
 
 let respuesta, respuestaSwitch
-
+//Consulto el tipo de búsqueda
 do {
     respuesta = parseFloat(prompt(" Ingrese un numero dependiendo del tipo de búsqueda que desea hacer\n Si desea buscar según el año ingrese 1\n Si desea buscar según su categoría ingrese 2\n Si desea buscar según su duración presione 3\n Si desea buscar según su puntuación en Rotten Tomatoes ingrese 4 "))
     if ((respuesta != 1) && (respuesta!= 2) && (respuesta != 3) && (respuesta != 4)){
@@ -49,40 +52,40 @@ do {
 } while((respuesta != "1") &&(respuesta != 2) && (respuesta != 3) && (respuesta != 4))
 
 
-
+//Switch según la respuesta del tipo de búsqueda
 switch(respuesta){
-    case 1 :
-        do{
+    case 1 : 
+        do{ // pido que ingrese el año de la película
             respuestaSwitch = parseFloat(prompt("Ingrese el año de la película que quiere ver, las opciones son van entre 2017 y 2022\n (Los resultados se podrán ver en consola)"))
         } while (respuestaSwitch <= 2016 || respuestaSwitch>=2023)
-
+        // muestro las películas en las que el año coincida con lo que el usuario ingresó
         const peliculasFiltradasAño = peliculas.filter(pelicula => pelicula.año == respuestaSwitch)
         peliculasFiltradasAño.forEach(pelicula => {
             console.log(`Quizá pueda interesarte la película ${pelicula.nombre}, es de ${pelicula.categoría},dura ${pelicula.duracion} minutos y recibío ${pelicula.puntuacion} puntos en Rotten Tomatoes`)
         })
         break
     case 2 :
-        do{
+        do{ // pido que ingrese categoría
             respuestaSwitch = prompt("Ingrese la categoría que quiere ver, las opciones son : drama, sobrenatural, misterio, crimen, ciencia-ficcion, suspenso, deportes,belico, aventura, comedia\n (Los resultados se podrán ver en consola)").toLocaleLowerCase()
         } while (respuestaSwitch!= "drama" && respuestaSwitch!= "misterio" && respuestaSwitch!= "crimen" && respuestaSwitch!= "ciencia-ficcion" && respuestaSwitch!= "suspenso" && respuestaSwitch!= "deportes" && respuestaSwitch!= "belico" && respuestaSwitch!= "aventura" && respuestaSwitch!= "comedia")
-
+        // si se encuentra en el array de categorías lo que el usuario ingresó, que se muestre la frase con info del objeto.
         const peliculasFiltradasCategoria = peliculas.filter(pelicula => pelicula.categoria.includes(respuestaSwitch) == true)
         peliculasFiltradasCategoria.forEach(pelicula => {console.log(`Quizá pueda interesarte la película ${pelicula.nombre} del año ${pelicula.año}, es de ${pelicula.categoria},dura ${pelicula.duracion} minutos y recibió %${pelicula.puntuacion} de puntaje en Rotten Tomatoes`)
         })
         break
     case 3 :
-        do{
+        do{ // pido que ingrese 1 o 2 dependiendo la duracion de la pelicula
             respuestaSwitch = parseFloat(prompt("Ingrese 1 si desea ver una película que dure menos de dos horas\n Ingrese 2 si desea ver una película que dure más de dos horas"))
         } while (respuestaSwitch != 1 && respuestaSwitch != 2)
         switch(respuestaSwitch){
-            case 1 :
+            case 1 : // si ingresa uno, muestro las peliculas que duren menos de 120 minutos
                 const peliculasFiltradasDuracion1 = peliculas.filter(pelicula => pelicula.duracion < 120)
         peliculasFiltradasDuracion1.forEach(pelicula => {
             console.log(`Quizá pueda interesarte la película ${pelicula.nombre}, es de ${pelicula.categoría},dura ${pelicula.duracion} minutos y recibío ${pelicula.puntuacion} puntos en Rotten Tomatoes`)
         })
             break
-            case 2 :
-                const peliculasFiltradasDuracion2 = peliculas.filter(pelicula => pelicula.duracion > 120)
+            case 2 : // si ingresa dos, muestro las peliculas que duren mas de 120 minutos
+                const peliculasFiltradasDuracion2 = peliculas.filter(pelicula => pelicula.duracion >= 120)
         peliculasFiltradasDuracion2.forEach(pelicula => {
             console.log(`Quizá pueda interesarte la película ${pelicula.nombre}, es de ${pelicula.categoría},dura ${pelicula.duracion} minutos y recibío ${pelicula.puntuacion} puntos en Rotten Tomatoes`)
         })
@@ -90,10 +93,10 @@ switch(respuesta){
         }
     break
     case 4 :
-        do{
+        do{ // pido ingresar puntaje entre el 89 y 100
             respuestaSwitch = parseFloat(prompt("Para buscar según el puntaje ingrese un número entre el 89 y el 100  \n (Los resultados se podrán ver en consola)"))
         } while (respuestaSwitch < 89 || respuestaSwitch > 100)
-
+        // muestro la info de las películas que coincidan con la respuesta del usuario 
         const peliculasFiltradas4 = peliculas.filter(pelicula => pelicula.puntuacion == respuestaSwitch)
         peliculasFiltradas4.forEach(pelicula => {
             console.log(`Quizá pueda interesarte la película ${pelicula.nombre}, es de ${pelicula.categoría},dura ${pelicula.duracion} minutos y recibío ${pelicula.puntuacion} puntos en Rotten Tomatoes`)
